823bd76259989fc114709a9d67dcc698ccf41729
Lukas Grahl <lukas.g@amkat.de>
2022-11-21 16:35:49 +0100

    added loading files

--- settings.py
+++ settings.py
@@ -1,15 +1,12 @@
 import os
 
-RANDOM_SEED = 101
-PROJ_ROOT = os.path.dirname(os.path.abspath(__file__))
-DATA_DIR = os.path.join(PROJ_ROOT, "data")
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+DATA_DIR = os.path.join(PROJECT_ROOT, 'data')
 
+def check_make_data_dir(root, data_dir):
+	if os.path.isdir(os.path.join(root, data_dir)) is False:
+		os.makedirs(os.path.join(root, data_dir))
+	pass
 
-def make_check_data_dir(root: str, data_dir: str):
-    if os.path.isdir(os.path.join(root, data_dir)) is False:
-        os.makedirs(os.path.join(root, data_dir))
-    pass
-
-
-if __name__ == "__main__":
-    make_check_data_dir(PROJ_ROOT, DATA_DIR)
+if __name__ == '__main__':
+	check_make_data_dir(PROJECT_ROOT, DATA_DIR)
