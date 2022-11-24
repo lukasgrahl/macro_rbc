01ce180e2c1d44a9d7492e764d32eea0ef58914b
Lukas Grahl <lukas.g@amkat.de>
2022-11-24 09:33:00 +0100

    added time format func to utils

---
+++ src/utils.py
@@ -0,0 +1,7 @@
+import datetime as dt
+import pandas as pd
+
+def time_format(arr: pd.Series,
+                t_format: str = "%Y-%m-%d"):
+    arr = arr.astype(str)
+    return arr.apply(lambda x: dt.datetime.strptime(str(x), t_format))
