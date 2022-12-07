d17f29e332dadfbf1cacf2a69f43120da11c3566
Lukas Grahl <lukas.g@amkat.de>
2022-12-07 15:58:56 +0100

    Updated with simpler Kalman Filter models

--- src/utils.py
+++ src/utils.py
@@ -12,6 +12,57 @@ def time_format(arr: pd.Series,
     arr = arr.astype(str)
     return arr.apply(lambda x: dt.datetime.strptime(str(x), t_format))
 
+def make_var_names(var, n_lags, reg):
+    names = [f'L1.{var}']
+    for lag in range(1, n_lags + 1):
+        names.append(f'D{lag}L1.{var}')
+    if reg != 'n':
+        names.append('Constant')
+    if 't' in reg:
+        names.append('Trend')
+
+    return names
+
+def ADF_test_summary(df, maxlag=None, autolag='BIC', missing='error'):
+    if missing == 'error':
+        if df.isna().any().any():
+            raise ValueError("df has missing data; handle it or pass missing='drop' to automatically drop it.")
+            
+    if isinstance(df, pd.Series):
+        df = df.to_frame()
+        
+    for series in df.columns:
+        data = df[series].copy()
+        if missing == 'drop':
+            data.dropna(inplace=True)
+            
+        print(series.center(110))
+        print(('=' * 110))
+        line = 'Specification' + ' ' * 15 + 'Coeff' + ' ' * 10 + 'Statistic' + ' ' * 5 + 'P-value' + ' ' * 6 + 'Lags' + ' ' * 6 + '1%'
+        line += ' ' * 10 + '5%' + ' ' * 8 + '10%'
+        print(line)
+        print(('-' * 110))
+        spec_fixed = False
+        for i, (name, reg) in enumerate(zip(['Constant and Trend', 'Constant Only', 'No Constant'], ['ct', 'c', 'n'])):
+            stat, p, crit, regresult = sm.tsa.adfuller(data, regression=reg, regresults=True, maxlag=maxlag,
+                                                       autolag=autolag)
+            n_lag = regresult.usedlag
+            gamma = regresult.resols.params[0]
+            names = make_var_names(series, n_lag, reg)
+            reg_coefs = pd.Series(regresult.resols.params, index=names)
+            reg_tstat = pd.Series(regresult.resols.tvalues, index=names)
+            reg_pvals = pd.Series(regresult.resols.pvalues, index=names)
+
+            line = f'{name:<21}{gamma:13.3f}{stat:15.3f}{p:13.3f}{n_lag:11}{crit["1%"]:10.3f}{crit["5%"]:12.3f}{crit["10%"]:11.3f}'
+            print(line)
+
+            for coef in reg_coefs.index:
+                if coef in name:
+                    line = f"\t{coef:<13}{reg_coefs[coef]:13.3f}{reg_tstat[coef]:15.3f}{reg_pvals[coef]:13.3f}"
+                    print(line)
+    pass
+
+
 def plot_df(df: pd.DataFrame,
             row_ratio: float =1.5,
             figsize_col: int=10):
@@ -51,11 +102,11 @@ def plot_sm_results(res, filter_output='predicted'):
         lower = mu - 1.98 * np.sqrt(sigma + 1e-8)
 
         start_idx = 1 if filter_output == 'predicted' else 0
-        axis.plot(mle_res.data.dates, mu.values[start_idx:], label='Predicted')
-        axis.fill_between(mle_res.data.dates, lower.values[start_idx:], upper.values[start_idx:], color='tab:blue', alpha=0.25)
+        axis.plot(res.data.dates, mu.values[start_idx:], label='Predicted')
+        axis.fill_between(res.data.dates, lower.values[start_idx:], upper.values[start_idx:], color='tab:blue', alpha=0.25)
 
         if name in endog_vars:
-            mle_res.data.orig_endog[name].plot(label='Data', ax=axis)
+            res.data.orig_endog[name].plot(label='Data', ax=axis)
 
         axis.set(title=name)
     fig.tight_layout()
@@ -65,7 +116,7 @@ def plot_sm_results(res, filter_output='predicted'):
 
     plt.show()
 
-
+    
 def skipna(func): 
 
     @wraps(func)
@@ -81,7 +132,21 @@ def skipna(func):
     return wrapper
 
 
-
+@skipna
+def poly_detrend(arr, poly_order: int=2, return_pred: bool = False, return_func_form: bool=False, **kwargs):
+    x = range(0, len(arr))
+    y = arr.values
+    model = np.polyfit(x, y, poly_order, **kwargs)
+    predicted = np.polyval(model, x)
+    detrend = y - predicted
+    if return_pred:
+        return predicted
+    elif return_func_form:
+        return model
+    else:
+        return detrend
+    
+    
 @skipna
 def apply_func(arr, func, **kwargs):
     """
@@ -92,8 +157,8 @@ def apply_func(arr, func, **kwargs):
     skipna: wether to skipna or note
     func: transformation function
     """
-    return func(arr)
-
+    return func(arr, **kwargs)
+    
 
 @skipna
 def get_seasonal_decompose(arr: pd.Series, plot: bool=False, **kwargs):
@@ -126,16 +191,3 @@ def arr_adf(arr, maxlag: int=10, p_level: float=.05):
     test = adfuller(arr, maxlag=maxlag)
     print(arr.name, f" p-val: {test[1]},  reject: {test[1] <= p_level}")
     pass
-    
-
-@skipna
-def poly_detrend(arr, poly_order: int=2, return_pred: bool = False, **kwargs):
-    x = range(0, len(arr))
-    y = arr.values
-    model = np.polyfit(x, y, poly_order, **kwargs)
-    predicted = np.polyval(model, x)
-    detrend = y - predicted
-    if return_pred:
-        return predicted
-    else:
-        return detrend
