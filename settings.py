495a7f45075e40fa16181db301723013a0144bb2
Lukas Grahl <lukas.g@amkat.de>
2022-12-10 11:26:30 +0100

    Correct outputs for all notebooks 01-05, please comment

--- settings.py
+++ settings.py
@@ -2,11 +2,3 @@ import os
 
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 DATA_DIR = os.path.join(PROJECT_ROOT, 'data')
-
-def check_make_data_dir(root, data_dir):
-	if os.path.isdir(os.path.join(root, data_dir)) is False:
-		os.makedirs(os.path.join(root, data_dir))
-	pass
-
-if __name__ == '__main__':
-	check_make_data_dir(PROJECT_ROOT, DATA_DIR)
